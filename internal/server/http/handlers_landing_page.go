9c150b08b5f9f1b21d3582f74bb455189d7e67bf
Shayne Preston <shayne.preston@protonmail.com>
2024-02-07 15:20:28 -0500

    refactor(templates): follow templ guidelines

--- internal/server/http/handlers_landing_page.go
+++ internal/server/http/handlers_landing_page.go
@@ -3,21 +3,15 @@ package http
 import (
 	"net/http"
 
-	"github.com/prests/htmx-portfolio/internal/server/http/web/components"
 	"github.com/prests/htmx-portfolio/internal/server/http/web/templates"
 )
 
-type landingPageProps struct {
-	Title string
-	Name  string
-}
-
 func (h *Handlers) landingPageHandler(w http.ResponseWriter, r *http.Request) {
-	props := &landingPageProps{
+	props := &templates.LandingPageProps{
 		Title: "Test",
 		Name:  "Shayne",
 	}
 
-	tmpl := templates.BaseHTML(props.Title, components.Hello(props.Name))
+	tmpl := templates.LandingPage(props)
 	tmpl.Render(r.Context(), w)
 }
