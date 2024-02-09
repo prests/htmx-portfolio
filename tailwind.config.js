e57ef00d7d4c3dcb89344693f8dfce8409048858
Shayne Preston <shayne.preston@protonmail.com>
2024-02-09 09:28:58 -0500

    style(landing page): start looking like vue original

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,14 @@ module.exports = {
     "./internal/server/http/web/templates/**/*.templ",
   ],
   theme: {
-    color: {
-      "orange-red-crystal": "#fe5f55",
-      "light-coral": "#263747",
-      "custom-grey": "#aaaeb4",
-      "custom-white": "#f7f7ff",
-      "black-coral": "#2f3f4e",
+    extend: {
+      colors: {
+        "orange-red-crystal": "#fe5f55",
+        "light-coral": "#263747",
+        "light-grey": "#aaaeb4",
+        "custom-white": "#f7f7ff",
+        "black-coral": "#2f3f4e",
+      },
     },
   },
   plugins: [],
